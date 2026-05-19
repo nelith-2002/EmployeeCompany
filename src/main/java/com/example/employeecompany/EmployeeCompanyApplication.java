2012727369f4ecef768ddf1ecc721b70a6b57abc
Nelith Nethsanda <nelithnethsanda2002@gmail.com>
2026-05-19 17:14:47 +0530

    feat: initialized the project

---
+++ src/main/java/com/example/employeecompany/EmployeeCompanyApplication.java
@@ -0,0 +1,13 @@
+package com.example.employeecompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeCompanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeCompanyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/employeecompany/EmployeeCompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.employeecompany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeCompanyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
